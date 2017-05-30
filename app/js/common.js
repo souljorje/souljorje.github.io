b67a24e9ffbb837b5a53656e45944644ba34c019
Georgiy Bukharov <souljorje@gmail.com>
2017-05-30 18:24:20 +0500

    before expirement

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,19 @@
 new Cocoen(document.querySelector('.cocoen'));
+
+$('.feedback-slider').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 10000,
+  arrows: true,
+  prevArrow: "<div class='prev'></button",
+  nextArrow: "<div class='next'></button>",
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+});
